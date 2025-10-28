b6d9980348edb7cea6c0d516a76bcd2d1e9723bf
BEE2V <135147675+BEE2V@users.noreply.github.com>
2025-10-28 17:46:12 +0530

    Tester 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 #include "LEDline.h"
 // LEDline line1(6, 46);
 
-LEDline line1(3, 150);
+LEDline line1(11, 150);
+LEDline line2(13, 125);
+LEDline line3(9, 125);
+LEDline line4(7, 125);
+LEDline line5(5, 125);
+LEDline line6(3, 125);
 // LEDline line2(7, 130);
 
 long pulse = 0;
@@ -25,20 +30,38 @@ void setup()
 
   // line1.addsegment(0, 39);
   line1.begin();
+  line1.addsegment(0, 29);
+  line1.addsegment(29, 29);
+  line1.addsegment(58, 44);
+  line1.addsegment(102, 23);
 
-  // pinMode(5, OUTPUT);    // Pin for the matrix
-  // digitalWrite(5, HIGH); // Set the pin to HIGH (255) to power the matrix
-  // matrix.addPingPoint(4, 7);
-  // matrix.addPingPoint(6, 2);
-  // matrix.addPingPoint(1, 5);
-  // matrix.addPingPoint(2,3);
+  line2.begin();
+  line3.begin();
+  line4.begin();
+  line5.begin();
+  line6.begin();
+
+  for (int i = 0; i < 4; i++)
+  {
+    line1
+  }
 }
 
 int com_len = 5;
 void loop()
 {
 
-  line1.commet(0, RED, BLUE, com_len, pulse, false, 150);
+  // line1.commet(0, RED, BLUE, com_len, pulse, false, 50);
+  line1.commet(1, RED, BLUE, com_len, pulse, false, 50);
+  line1.commet(2, RED, BLUE, com_len, pulse, false, 50);
+  line1.commet(3, RED, BLUE, com_len, pulse, false, 50);
+  line1.commet(4, RED, BLUE, com_len, pulse, false, 50);
+
+  // line2.commet(0, RED, BLUE, com_len, pulse, false, 100);
+  // line3.commet(0, RED, BLUE, com_len, pulse, false, 100);
+  // line4.commet(0, RED, BLUE, com_len, pulse, false, 100);
+  // line5.commet(0, RED, BLUE, com_len, pulse, false, 100);
+  // line6.commet(0, RED, BLUE, com_len, pulse, false, 100);
   // line1.commet(1, RED, BLUE, com_len, pulse, true, 58);
   // line1.commet(2, RED, BLUE, com_len, pulse, false, 21);
 
@@ -52,6 +75,6 @@ void loop()
   // line2.commet(3, RED, BLUE, 10, pulse + 35, true, 58);
   // line2.commet(4, RED, BLUE, 10, pulse + 35, false, 58);
 
-  delay(10);
+  delay(2);
   pulse++;
 }
